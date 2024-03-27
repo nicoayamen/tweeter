e376f490c867b5bce1793998dec8c6fff212aaaf
nicoayamen <nicolas.ayala4@gmail.com>
2024-03-26 21:07:38 -0600

    resolve issue with tweets not showing unless GET method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,22 +6,41 @@
 
 $(document).ready(function() {
 
-  //event listener to submit POST request asynchronously
+  // Function to load tweets on initial page load
+  const loadTweets = function() {
+    // GET request to load tweets using renderTweets function
+    $.get("/tweets")
+      .then((res) => {
+        renderTweets(res);
+      });
+  };
+
+  // Function to render tweets onto the page
+  const renderTweets = function(tweets) {
+    // Loop through tweets
+    tweets.forEach(tweet => {
+      // Call createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // Append tweet to the tweets container
+      $('.tweet-container').prepend($tweet);
+    });
+  };
+
+  // Event listener to submit POST request asynchronously
   $("#tweet-form").on("submit", function(event) {
-    //prevent default behaviour
+    // Prevent default behaviour
     event.preventDefault();
 
-
     const isTweetValid = function() {
-      //define variable for number of characters in tweet text form with whitespace trimmed
+      // Define variable for number of characters in tweet text form with whitespace trimmed
       let charCount = $(".tweet-text").val().trim().length;
 
-      //display error if no text is entered
+      // Display error if no text is entered
       if (charCount === 0) {
         $(".new-tweet-alert").text("Write something! Let the world!").slideDown("slow");
         return false;
       }
-      //display error if character count exceeds 140
+      // Display error if character count exceeds 140
       if (charCount > 140) {
         $(".new-tweet-alert").text("You cannot post a tweet more than 140 characters long!").slideDown("slow");
         return false;
@@ -31,55 +50,41 @@ $(document).ready(function() {
       return true;
     };
 
-    //if tweet is valid, continue with POST request
+    // If tweet is valid, continue with POST request
     if (isTweetValid()) {
-      //turn form data into a query string
+      // Turn form data into a query string
       let formData = $(this).serialize();
-      //POST request to send serialized data to server
+      // POST request to send serialized data to server
       $.post("/tweets", formData)
         .then(() => {
+          // After successful post, load tweets again to update the view
           loadTweets();
+          // Clear tweet text area and reset character counter
           $(".tweet-text").val("");
           $(".counter").text("140");
         });
     }
   });
 
-  const loadTweets = function() {
-    //GET request to load tweets using renderTweets function
-    $.get("/tweets")
-      .then((res) => {
-        renderTweets(res);
-      });
-  };
+  // Call loadTweets when the document is ready to fetch tweets on initial page load
+  loadTweets();
 
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    tweets.forEach(tweet => {
-      // calls createTweetElement for each tweet
-      const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('.tweet-container').prepend($tweet);
-    });
-  };
-
-  //sanitize incoming text inputs
+  // Sanitize incoming text inputs
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  //function to take object with tweet data and return an HTML article containing tweet info
+  // Function to create HTML structure for a tweet
   const createTweetElement = function(tweet) {
-
-    //Utilize timeAgo library to format tweet submission dates
+    // Utilize timeAgo library to format tweet submission dates
     const timeAgo = timeago.format(tweet.created_at, 'en_US');
 
-    //define tweet based on object
-    const { user: { name, avatars, handle }, content: { text }, created_at } = tweet;
+    // Define tweet based on object
+    const { user: { name, avatars, handle }, content: { text } } = tweet;
 
-    //define tweet HTML with applicable info from data above
+    // Define tweet HTML with applicable info from data above
     let $tweet = $(`
     <article class="tweet">
       <header class="tweet-header">
@@ -90,12 +95,12 @@ $(document).ready(function() {
         <div class="tweet-handle">${handle}</div>
       </header>
       <div class="tweet-content">
-      <div>${escape(text)}</div>
+        <div>${escape(text)}</div>
       </div>
-      <div class = "tweet-border"></div>
+      <div class="tweet-border"></div>
       <footer class="tweet-footer">
         <div class="tweet-date">
-        <p>${timeAgo}</p>
+          <p>${timeAgo}</p>
         </div>
         <div class="tweet-icons">
           <i class="fa-solid fa-flag"></i>
@@ -106,7 +111,7 @@ $(document).ready(function() {
     </article>
   `);
 
-    //return tweet in HTML form
+    // Return tweet in HTML form
     return $tweet;
   };
 
